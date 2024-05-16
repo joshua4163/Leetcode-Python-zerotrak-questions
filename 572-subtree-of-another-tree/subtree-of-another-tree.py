e59430222d76336c894469f6c0c150207896d1f1
Joshua <78430670+joshua4163@users.noreply.github.com>
2024-05-16 18:02:04 -0500

    Time: 70 ms (78.23%) | Memory: 16.7 MB (47.66%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -1,12 +1,27 @@
-class Solution:
-    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        if not s: 
-            return False
-        if self.isSameTree(s, t): 
-            return True
-        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p and q:
-            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        return p is q
+class Solution:
+    def isSubtree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+        if not t:  # if t is None
+            return True
+        if not s:  # if s is None
+            return False
+        if self.isSameTree(s, t):  # check if trees are identical
+            return True
+        # check subtrees
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+    
+    def isSameTree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+        if not s and not t:  # both are None
+            return True
+        if not s or not t:  # one is None, the other is not
+            return False
+        if s.val != t.val:  # values don't match
+            return False
+        # recursively check left and right subtrees
+        return self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
