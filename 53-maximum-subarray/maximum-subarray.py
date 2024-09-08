f41c922785ee77b8b373631752c969155dc52568
Joshua <78430670+joshua4163@users.noreply.github.com>
2024-09-08 00:55:33 -0500

    Time: 550 ms (30.34%) | Memory: 31 MB (70.80%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,9 +1,16 @@
+from typing import List
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxi = float('-inf')
-        temp = 0
-        for i in nums:
-            temp += i 
-            maxi = max(maxi,temp)
-            temp = 0 if temp <0 else temp
-        return maxi
+        # Initialize the global and current sum
+        globalsum = nums[0]
+        currsum = nums[0]
+        
+        # Iterate over the array starting from the second element
+        for i in range(1, len(nums)):
+            # Update currsum to either the current element or currsum + current element
+            currsum = max(nums[i], currsum + nums[i])
+            # Update globalsum if currsum is greater
+            globalsum = max(globalsum, currsum)
+        
+        return globalsum
