117904e5c12b84006b35db16650e198c87ab589c
Joshua <78430670+joshua4163@users.noreply.github.com>
2024-09-25 00:00:50 -0500

    Time: 472 ms (28.99%) | Memory: 27.9 MB (88.74%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -3,7 +3,7 @@ class Solution:
         l = 0
         window = set()
         for r in range(len(nums)):
-            if r-l > k:
+            if r - l > k:
                 window.remove(nums[l])
                 l += 1
             if nums[r] in window:
